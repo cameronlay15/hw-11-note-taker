4088617a835791a2ff71d62f20dba4a2feb55145
Cameron Lay <cameronlay15@gmail.com>
2020-10-01 19:36:35 -0400

    express use

--- server.js
+++ server.js
@@ -3,3 +3,11 @@ const fs = require("fs");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+})
